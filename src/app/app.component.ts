e3726532eb807fc2cc23c6aa30118ab59176fbb4
Prithivi Raj G <prithiviraj1991@gmail.com>
2018-04-27 11:48:41 +0530

    new build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,40 @@ export class AppComponent {
   title = 'app';
 
   constructor(){
-    gapi.load('auth2', function () {
-      gapi.auth2.init()
-   });
+    
+  }
+
+  public auth2: any;
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '887948310699-eo004dkpmerbq9gdh50pn81vdafj33vr.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('googleBtn'));
+    });
+  }
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        //YOUR CODE HERE
+
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
   }
 
   ngAfterViewInit() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'light',
-        'onsuccess': param => this.onSignIn(param)
-    });
+    this.googleInit();
 }
 
   onSignIn(googleUser) {
